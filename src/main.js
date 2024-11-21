78a99390d8228bcf5df39fb4d89f1038587bf808
vineboneto <vineboneto@gmail.com>
2024-11-21 05:33:07 -0300

    feat: testando limites para receber arquivo em stream em fastify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Fastify from "fastify";
 import fs from "node:fs";
 import path from "node:path";
+import { pipeline } from "node:stream/promises";
 import cors from "@fastify/cors";
+import multipart from "@fastify/multipart";
 import { monitorMemory } from "./memory.js";
+import { v7 as uuidv7 } from "uuid";
 import { generateExcelFile } from "./excel.js";
 import {
 	queue,
@@ -12,10 +15,18 @@ import {
 	queueFork,
 } from "./excel-run.js";
 
-monitorMemory();
+// monitorMemory();
 
 const app = Fastify({
 	logger: true,
+});
+
+app.register(multipart, {
+	limits: {
+		// fileSize: 100 * 1024 * 1024, // Aumenta o limite para 100MB
+		// parts: Number.POSITIVE_INFINITY,
+		// fileSize: Number.POSITIVE_INFINITY,
+	},
 });
 
 app.register(cors, {
@@ -35,6 +46,53 @@ app.post("/write-file", async (req, reply) => {
 	} catch (err) {
 		console.log(err);
 		throw err;
+	}
+});
+
+/**
+ * Recebe o arquivo e escreve de uma vez
+ */
+app.post("/send-file", async (req, reply) => {
+	try {
+		const data = await req.file();
+
+		// Caminho onde o arquivo será salvo no servidor
+		const filePath = path.join("public", data.filename);
+
+		// Escrevendo o arquivo no disco
+		await fs.promises.writeFile(filePath, await data.toBuffer());
+
+		reply.send();
+	} catch (err) {
+		console.log(err);
+		throw err;
+	}
+});
+
+/**
+ * Recebe o arquivo (o arquivo precisar ser recebido inteiro) e escreve em stream
+ */
+app.post("/send-file-stream", async (req, reply) => {
+	try {
+		const filePath = path.join("public", "great-size-file.xlsx");
+		const writeStream = fs.createWriteStream(filePath);
+
+		for await (const part of req.parts({
+			limits: { fileSize: Number.POSITIVE_INFINITY },
+		})) {
+			if (part.type === "file") {
+				await pipeline(part.file, writeStream);
+				console.log("Parte de 1MB escrita. Aguardando 1 segundo...");
+			}
+		}
+
+		// Respondendo com sucesso
+		reply.send({ message: "Arquivo recebido e salvo com sucesso!" });
+	} catch (err) {
+		console.error("Erro ao processar o arquivo:", err);
+		reply
+			.status(500)
+			.send({ error: "Erro ao processar o arquivo", message: err.message });
 	}
 });
 
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import fs from "node:fs";
+import FormData from "form-data";
 import { AxiosError } from "axios";
 
 const api = axios.create({
@@ -65,6 +66,70 @@ export async function writeFile() {
 	console.timeEnd("write");
 }
 
+export async function sendFile() {
+	console.time("send-file");
+
+	const form = new FormData();
+
+	const filePath = "./great-size-file.xlsx";
+	const file = await fs.promises.readFile(filePath);
+
+	form.append("file", file, "great-size-file.xlsx");
+
+	try {
+		const response = await api.post("/send-file", form, {
+			headers: {
+				...form.getHeaders(),
+			},
+		});
+
+		console.log("Arquivo enviado com sucesso!", response.status, response.data);
+	} catch (err) {
+		if (err instanceof AxiosError) {
+			console.error("Erro ao enviar o arquivo:", err.response?.data);
+		}
+	}
+
+	console.timeEnd("send-file");
+}
+
+export async function sendFileStream() {
+	console.time("send-file-stream");
+
+	const form = new FormData();
+
+	// Caminho do arquivo
+	const filePath = "./great-size-file.xlsx";
+
+	// Lendo o arquivo como stream
+	const fileStream = fs.createReadStream(filePath);
+
+	form.append("file", fileStream, {
+		contentType: "binary/octet-stream",
+		// knownLength: (await fs.promises.stat(filePath)).size,
+		filename: "./great-size-file.xlsx",
+	});
+
+	try {
+		const response = await axios.post(
+			"http://localhost:3333/send-file-stream",
+			form,
+			{
+				headers: {
+					...form.getHeaders(),
+				},
+			},
+		);
+
+		console.log("Arquivo enviado com sucesso!", response.status, response.data);
+	} catch (err) {
+		console.log(err.message);
+		console.error("Erro ao enviar o arquivo:", err.response?.data);
+	}
+
+	console.timeEnd("send-file-stream");
+}
+
 async function main() {
 	const [, , command] = process.argv; // Pega o terceiro argumento da linha de comando
 
@@ -78,9 +143,15 @@ async function main() {
 		case "writeFile":
 			await writeFile();
 			break;
+		case "sendFile":
+			await sendFile();
+			break;
+		case "sendFileStream":
+			await sendFileStream();
+			break;
 		default:
 			console.log(
-				"Comando inválido. Use 'streamFile', 'writeFileParallel' ou 'writeFile'.",
+				"Comando inválido. Use 'streamFile', 'writeFileParallel', 'sendFile', 'sendFileStream' ou 'writeFile'.",
 			);
 			break;
 	}
